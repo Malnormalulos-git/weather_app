e58807a140de380213a0ba7c462f63bd6bd88bd3
unknown <malnormalulo1@gmail.com>
2024-04-29 17:55:51 +0300

    added tabs to root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,47 @@
-import { AppBar, Toolbar, Typography } from "@mui/material"
+import { styled } from '@mui/material/styles';
+import { AppBar, Box, Toolbar, Typography } from "@mui/material"
 import Forecast from "./components/Forecast"
+import { useState } from "react";
+import Tab from '@mui/material/Tab';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
 
 const App = () => {
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <>
-    <AppBar position="static">
-      <Toolbar variant="dense">
-        <Typography 
-          variant="h4"
-        >
-          WeatherReporter
-        </Typography>
-      </Toolbar>
-    </AppBar>
-    feed <br/>
-    <Forecast city="Kyiv"/>
+      <Box
+        bgcolor={"background.default"}
+        color={"text.primary"}
+        sx={{minHeight: '100vh'}}
+      >
+        <AppBar position="static">
+          <Toolbar variant="dense">
+          <Box display="flex" justifyContent="center" width="100%">
+            <Typography variant="h4">WeatherReporter</Typography>
+          </Box>
+          </Toolbar>
+        </AppBar>
+
+        <TabContext value={value}>
+          <Box 
+            sx={{ 
+              borderBottom: 1, 
+              borderColor: 'divider' 
+            }}
+          >
+            <TabList onChange={handleChange} aria-label="Forecast tabs" variant="fullWidth">
+              <Tab label="Zaporizhzhia" value="1" />
+              <Tab label="Kyiv" value="2" />
+            </TabList>
+          </Box>
+          <TabPanel value="1"><Forecast lat="47.8508" lon="35.1183"/></TabPanel>
+          <TabPanel value="2"><Forecast lat="50.45466" lon="30.5238"/></TabPanel>
+        </TabContext>
+      </Box>
     </>
   )
 }
